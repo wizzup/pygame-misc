59d73e5e31c2dd92b8e1fdea6e2ec0e94165d8eb
wisut hantanong <wizzup@gmail.com>
2021-07-27 21:01:46 +0700

    save/load to a file

--- game.py
+++ game.py
@@ -29,6 +29,7 @@ class Game(object):
                              # use t.get_ticks() for elapse time since init
         self.state = GameState.RUN
         self.target = (SCREEN_CENTER)
+        self.file_name = 'save.map'
 
     def main_loop(self):
 
@@ -39,6 +40,8 @@ class Game(object):
             for e in g.event.get(g.KEYDOWN):
                 if e.key == g.K_ESCAPE:
                     self.state = GameState.QUIT
+                elif e.key == g.K_s:
+                    save()
                 else:
                     g.event.post(e)
 
@@ -53,7 +56,7 @@ class Game(object):
                     s.fill('white')
                     r = s.get_rect()
                     r.topleft = (i,j)
-                    grids.append((s,r))
+                    grids.append([s,r,'W'])
             return grids
 
         def draw_grids():
@@ -61,8 +64,27 @@ class Game(object):
                 g.draw.line(self.screen,'black',(0,i),(SCREEN_WIDTH, i))
                 g.draw.line(self.screen,'black',(i,0),(i,SCREEN_HEIGH))
 
+        def save():
+            with open(self.file_name, 'w') as f:
+                for g in grids:
+                    f.write(g[2])
+
+        def load():
+            with open(self.file_name, 'r') as f:
+                for g in grids:
+                    c = f.read(1)
+                    if c == 'W':
+                        g[0].fill('white')
+                        g[2] = 'W'
+                    elif c == 'B':
+                        g[0].fill('black')
+                        g[2] = 'B'
+
+
+
         clock = t.Clock()
         grids = make_grids()
+        load()
 
         # drawing loop
         while self.state == GameState.RUN:
@@ -81,8 +103,10 @@ class Game(object):
                 mp = g.mouse.get_pos()
                 if mb[0] and i[1].collidepoint(mp):
                     i[0].fill('black')
+                    i[2] = 'B'
                 if mb[2] and i[1].collidepoint(mp):
                     i[0].fill('white')
+                    i[2] = 'W'
                 self.screen.blit(i[0], i[1])
 
             draw_grids()
